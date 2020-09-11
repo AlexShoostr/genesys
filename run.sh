a9113f26842de586b04351c7b22bae199ba7fdf3
Быстров Александр Дмитриевич <bad@sofit.tech>
2020-09-11 13:44:05 +0300

    fix

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build . -f Dockerfile.serverbox -t serverbox:0.0.1
-docker build . -f Dockerfile.clientbox -t clientbox:0.0.1
-./run-test.sh
--- script.sh
+++ script.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+
 echo $TESTVAR
 
 python3 ./script.py
 
 echo "Server hostname is" $HOSTNAME
 
-CLIENT_HOSTNAME=`cat /home/connect/clientbox-hostname`
+for (( i = 1; i <= 5; i++ ))
+do
+  if [ -f /home/connect/clientbox-hostname ]; then
+    CLIENT_HOSTNAME=`cat /home/connect/clientbox-hostname`
+  else
+    echo "Не могу получить получить файл. Попытка" $i
+    sleep 5
+  fi
+done
 
 echo "Client hostname is" $CLIENT_HOSTNAME
+
+if [ -f /home/connect/clientbox-hostname ]; then
+  rm /home/connect/clientbox-hostname
+fi
